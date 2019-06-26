3190c8366e33fce7f97eb28d249088e6834b177b
David Corwin <dshvazz@gmail.com>
2019-06-25 22:49:07 -0700

    Added mailgun

--- api/sun-event.js
+++ api/sun-event.js
@@ -1,4 +1,5 @@
 const { parse } = require('url');
+const { sendError } = require('../lib/send-error');
 const sunApi = require('../lib/sun-api');
 const { getRelayState, setRelayState } = require('../lib/tplink-api');
 
@@ -26,53 +27,58 @@ const timeFormat = new Intl.DateTimeFormat('en-US', {
 });
 
 module.exports = async (req, res) => {
-  const { deviceId, token } = parse(req.url, true).query;
+  try {
+    const { deviceId, token } = parse(req.url, true).query;
 
-  if (token !== API_KEY) {
-    res.writeHead(401);
-    return res.end();
-  }
-
-  let message = 'No op';
-  const log = {};
-
-  const currentDate = dateFormat.format();
-  const currentTime = timeFormat.format();
-
-  log.currentDate = currentDate;
-  log.currentTime = currentTime;
-
-  const [currentState, sunData] = await Promise.all([
-    getRelayState(deviceId),
-    sunApi(currentDate).then(({ sunrise, sunset }) => ({
-      sunrise: timeFormat.format(new Date(sunrise)),
-      sunset: timeFormat.format(new Date(sunset)),
-    })),
-  ]);
-
-  log.currentState = currentState;
-  log.sunrise = sunData.sunrise;
-  log.sunset = sunData.sunset;
-
-  const item = SCHEDULE.find(([timeStr]) => {
-    let _timeStr = timeStr;
-    if (['sunrise', 'sunset'].indexOf(timeStr) > -1) {
-      _timeStr = sunData[timeStr];
+    if (token !== API_KEY) {
+      res.writeHead(401);
+      return res.end();
     }
-    return currentTime > _timeStr;
-  });
 
-  if (item) {
-    log.item = item;
-    const lightState = item[1];
-    if (lightState !== currentState) {
-      await setRelayState(deviceId, lightState);
-      message = `Front light turned ${lightState}!`;
+    let message = 'No op';
+    const log = {};
+
+    const currentDate = dateFormat.format();
+    const currentTime = timeFormat.format();
+
+    log.currentDate = currentDate;
+    log.currentTime = currentTime;
+
+    const [currentState, sunData] = await Promise.all([
+      getRelayState(deviceId),
+      sunApi(currentDate).then(({ sunrise, sunset }) => ({
+        sunrise: timeFormat.format(new Date(sunrise)),
+        sunset: timeFormat.format(new Date(sunset)),
+      })),
+    ]);
+
+    log.currentState = currentState;
+    log.sunrise = sunData.sunrise;
+    log.sunset = sunData.sunset;
+
+    const item = SCHEDULE.find(([timeStr]) => {
+      let _timeStr = timeStr;
+      if (['sunrise', 'sunset'].indexOf(timeStr) > -1) {
+        _timeStr = sunData[timeStr];
+      }
+      return currentTime > _timeStr;
+    });
+
+    if (item) {
+      log.item = item;
+      const lightState = item[1];
+      if (lightState !== currentState) {
+        await setRelayState(deviceId, lightState);
+        message = `Front light turned ${lightState}!`;
+      }
     }
+    log.message = message;
+
+    console.log(log);
+
+    return res.end(message);
+  } catch (err) {
+    await sendError(err.message);
+    return res.end(err.message);
   }
-  log.message = message;
-
-  console.log(log);
-
-  return res.end(message);
 };
--- assets/relay-state.js
+++ assets/relay-state.js
@@ -1,13 +1,20 @@
 const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
 const form = document.forms[0];
-form.addEventListener('change', event => {
+form.addEventListener('change', async event => {
   const deviceId = event.target.dataset.deviceId;
   const state = event.target.checked ? 'on' : 'off';
-  fetch(`/api/set-relay-state/${deviceId}/${state}`, {
-    credentials: 'same-origin',
-    headers: {
-      'CSRF-Token': csrfToken,
-    },
-    method: 'POST',
-  });
+  try {
+    const response = await fetch(`/api/set-relay-state/${deviceId}/${state}`, {
+      credentials: 'same-origin',
+      headers: {
+        'CSRF-Token': csrfToken,
+      },
+      method: 'POST',
+    });
+    if (!response.ok) {
+      throw new Error(await response.text());
+    }
+  } catch (err) {
+    alert(err.message);
+  }
 });
